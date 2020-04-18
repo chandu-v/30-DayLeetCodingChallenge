6af33e467493898cb74da8538ab5d8b669524465
chandu-v <chandu_boss@hotmail.com>
2020-04-18 10:51:34 +0530

    Optimized Solution

--- Week1/SingleNumber.java
+++ Week1/SingleNumber.java
@@ -6,28 +6,23 @@ import java.util.Scanner;
 
 public class SingleNumber {
     public static void main(final String[] args) {
-    Scanner in = new Scanner(System.in);
-    int n = in.nextInt();
-    int arr[] = new int[n];
-    for(int i = 0 ; i < n ; i++){
-        arr[i] = in.nextInt();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+        System.out.println(singleNumber(arr));
     }
-    System.out.println(singleNumber(arr));
-}
 
-public static int singleNumber(final int[] nums) {
+    public static int singleNumber(final int[] nums) {
+        // Using XOR Operator, if a number xor with itself the result will be zero. so the final solution will be our answer.
+        // I got this solution from errichto's YouTube Channel.
+        int result = 0;
+        for (int i : nums) {
+            result ^= i;
 
-    HashMap<Integer,Integer> map = new HashMap<>();
-    
-    // Adding each number to the map and if it is found again I'm removing so that the number that finally remains is our output.
-    for(int i = 0 ; i < nums.length ; i++){
-       if(map.containsKey(nums[i])){
-            map.remove(nums[i]);
+        }
+        return result;
     }
-    else{
-        map.put(nums[i], 1);
-    }
-   }
-   return (int) map.keySet().toArray()[0];
-}
 }
