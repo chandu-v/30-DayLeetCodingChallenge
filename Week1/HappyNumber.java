4ebd61662f4e88e59ae09a7249308325b4616d63
chandu-v <chandu_boss@hotmail.com>
2020-04-18 11:28:00 +0530

    Accepted Solution - Optimized 100% of java solution

--- Week1/HappyNumber.java
+++ Week1/HappyNumber.java
@@ -15,8 +15,8 @@ public class HappyNumber {
         int digit = 0;
         while (sum != 1) {
             sum = 0;
-            if(n == 0   ||  n   ==  1   ||  n   ==  2   ||  n   ==  3){
-                if(n==1){
+            if(n/10 ==  0){
+                if(n==1 ||  n   ==  7){
                     return true;
                 }else{
                     return false;
@@ -28,6 +28,7 @@ public class HappyNumber {
                 n /= 10;
             }
             n = sum;
+            // System.out.println(sum);
             
         }
         result = true;
