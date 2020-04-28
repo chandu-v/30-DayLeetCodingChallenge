43ebd3c8f198b63279efc39fb30cee1c621fe9cd
chandu-v <chandu_boss@hotmail.com>
2020-04-28 16:02:40 +0530

    Time Limit Exceeded for SubArraySum

---
+++ Week3/LeftColumnWithAtleastOne.java
@@ -0,0 +1,8 @@
+package Week3;
+
+public class LeftColumnWithAtleastOne {
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Week4/SubArraySum.java
@@ -0,0 +1,42 @@
+package Week4;
+
+public class SubArraySum {
+
+    public static void main(String[] args) {
+        // [28,54,7,-70,22,65,-6]
+
+        // int [] arr = new int[]{1,2,3};
+        // int k = 3 ;
+        int [] arr = new int[]{28,54,7,-70,22,65,-6};
+        int k = 100 ;
+        int result = subarraySum(arr, k);
+        System.out.println(result);
+    }
+    public static int subarraySum(int[] nums, int k) {
+        int count = 0 ;
+        for(int i = 0 ; i < nums.length ; i++){
+            for(int j = i ; j < nums.length ; j++){
+                // System.out.println(String.format("%d, %d", i,j));
+                if(isSumEqualToK(nums, i, j, k)){
+                    // System.out.println(String.format("result Successed %d,%d", i,j));
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static boolean isSumEqualToK(int [] nums, int start , int end , int k){
+        boolean result = false;
+        int sum = 0;
+        for(int i = start ; i <= end ; i++){
+            
+            sum+=nums[i];
+        }
+        if(sum == k){
+            result = true;
+        }
+        System.out.println(String.format("%d, %d , %d",start, end, sum));
+        return result;
+    }
+}
